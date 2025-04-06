e87d0536afc7b9fc400c146b892d868971145b15
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-04-07 06:30:33 +0800

    Add sub readme

---
+++ readme.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+out=README.txt
+for dir in $(find * -type d)
+do
+	pushd $dir
+	rm -f $out
+	for file in *.txt
+	do
+		stem=${file%.*}
+		parts=(${stem//-/ })
+		book=${parts[0]^}
+		chap=${parts[1]}
+		chap=$((10#$chap))
+		echo "## $book $chap" >> $out
+		echo >> $out
+		addnl < $file >> $out
+	done
+	stripi $out
+	popd
+done
